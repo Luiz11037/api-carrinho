008abaa0fa8b74a65d924be258bdc6c3838f58a4
Luiz11037 <luizfelipe31felipe@gmail.com>
2025-09-19 15:28:49 -0300

    Adições

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
+#Instanciando app.
 app = Flask(__name__)
 
+#importando as rotas(GET, POST, DELETE, PUT)
 from routes import *
 
+#Isso precisa estar no final.
 if __name__ == "__main__":
     app.run(debug=True)
--- routes.py
+++ routes.py
@@ -1,2 +1,31 @@
+from flask import request
 from app import app
 from models import produtos
+
+"""
+POST /produto/save (criar produto)
+PUT /produto/{id}/update (atualizar produto)
+GET /produto/{id} (busca um produto)
+GET /produto/all (busca todos os produto)
+DELETE /produto/{id}/delete (deletar produto)
+POST /carrinho/addProduto/{id}?quantidade={quantidade} (adicionar produto no carrinho)
+DELETE /carrinho/removerProduto/1?quantidade={quantidade} (remover produto do carrinho)
+DELETE /carrinho/limpar (remover todos os produtos do carrinho)
+GET /carrinho/mostrar (busca o carrinho
+"""
+
+#Mensagem de boas vindas na raiz (/)
+@app.route("/")
+def home():
+    return{"messagem": "Bem vindo a api-carrinho"}
+
+#Mostrar lista de produtos
+@app.route("/produtos", methods=["GET"])
+def get_produtos():
+    return {"PRODUTOS": produtos}
+
+@app.route("/produtos", methods=["POST"])
+def post_produtos():
+    novo_produto = request.json
+    produtos.append(novo_produto)
+    return novo_produto
